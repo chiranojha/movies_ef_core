94094d7ef9b0d7b5420190fb083ae43c75b151f9
Chiranjeevee Ojha <chiranjeevee.ojha@selu.edu>
2019-05-28 20:46:39 -0500

    Initlializing new repository

---
+++ repos/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}

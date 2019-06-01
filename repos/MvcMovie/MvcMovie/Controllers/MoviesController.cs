5985e3ae6699412bb32822d343fdb49df344f633
Chiranjeevee Ojha <chiranjeevee.ojha@selu.edu>
2019-06-01 12:29:39 -0500

    added search by genre

--- repos/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ repos/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -19,8 +19,13 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // LINQ to get the list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
@@ -29,7 +34,26 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+            return View(movieGenreVM);
+        }
+         
+
+
+        [HttpPost]
+        public string Index(String searchString , bool notUsed)
+        {
+            return "From [httpPost] Index: filter on " + searchString;
+        
         }
 
         // GET: Movies/Details/5
